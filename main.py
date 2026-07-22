16fedd23a15cdc5c6833402c9812b74e2651eb39
anna derevianko <anna.derevianko.25@kse.org.ua>
2026-07-22 22:54:25 +0300

    finalized the main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import yfinance as yf #імпортували бібліотеку для роботи з фін.даними з YahooFinance
+from strategy import generate_signals
+from analytics import calculate_p_and_l
+from analytics import visualize
 
 #витягнули з YahooFinance дані про акції Netflix за рік, тикер - код інструменту на біржі (NFLX)
 historical_data = yf.download("NFLX", period="1y")
@@ -7,3 +10,6 @@ historical_data = yf.download("NFLX", period="1y")
 #перевіряємо DataFrame, на наявність відсутніх значень, і видаляємо їх
 historical_data_cleaned = historical_data.dropna()
 
+signals = generate_signals(historical_data_cleaned)
+calculate_p_and_l(signals)
+visualize(signals)
