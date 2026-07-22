3dc99fc40623415657a3d4bc130cae679c00efca
mariabodnarcuk <mariia.bodnarchuk.25@kse.org.ua>
2026-07-22 17:52:19 +0300

    analyzed if market is rising or falling and what actions we need to do

--- strategy.py
+++ strategy.py
@@ -2,9 +2,17 @@ import pandas as pd
 import numpy as np
 
 def genarate_signals(df: pd.DataFrame, short: int = 20, long: int = 100)-> pd.DataFrame:
-    data = df.copy() #копія таблиці данних з main
-    #рахуємо швидке та повілье просте ковзне середнє
+    data = df.copy() # копія таблиці данних з main
+    # рахуємо швидке та повілье просте ковзне середнє
     data["SMA_fast"] = data["Close"].rolling(window=short).mean()
     data["SMA_slow"] = data["Close"].rolling(window=long).mean()
+
+    # визначаємо стан ринку(чи він росте, чи падає)
+    data["Signal"] = 0.0
+    data["Signal"] = np.where(data["SMA_fast"] > data["SMA_slow"], 1.0, 0.0)
+
+    # визначаємо коли треба продавати(-1), коли купувати(+1), а коли чекати(0)
+    data["Action"] = data["Signal"].diff()
+    return data
 
 
