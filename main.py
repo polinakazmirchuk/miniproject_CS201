cc1158946bdc3ca8483eb0b4bb22809ba2b3361e
mariabodnarcuk <mariia.bodnarchuk.25@kse.org.ua>
2026-07-23 14:53:35 +0300

    test block for different periods made

--- main.py
+++ main.py
@@ -10,9 +10,8 @@ historical_data = yf.download("NFLX", period="1y")
 #перевіряємо DataFrame, на наявність відсутніх значень, і видаляємо їх
 historical_data_cleaned = historical_data.dropna()
 
-signals = generate_signals(historical_data_cleaned)
+signals = generate_signals(historical_data_cleaned, 9, 50)
 p_and_l = calculate_p_and_l(signals)
-
 print(f"Фінансовий результат стратегії за рік: {(p_and_l ['Accumulated return'].iloc[-1]-1)*100:.2f}%") # Беремо останній елемент з колонки Accumulated return. Вираховуємо чи приносить збиток чи прибуток.
 calculate_p_and_l(signals)
 visualize(signals)
--- strategy.py
+++ strategy.py
@@ -4,7 +4,7 @@ import yfinance as yf
 raw_data = yf.download("NFLX", period="1y")
 historical_data_cleaned = raw_data.dropna()
 
-def generate_signals(df: pd.DataFrame, short: int = 20, long: int = 100)-> pd.DataFrame:
+def generate_signals(df: pd.DataFrame, short: int, long: int)-> pd.DataFrame:
     data = df.copy() # копія таблиці данних з main
     # рахуємо швидке та повілье просте ковзне середнє
     data["SMA_fast"] = data["Close"].rolling(window=short).mean()
@@ -18,15 +18,25 @@ def generate_signals(df: pd.DataFrame, short: int = 20, long: int = 100)-> pd.Da
     data["Action"] = data["Signal"].diff()
     return data
 
-# передаємо очищені дані в функцію
-df_with_signals = generate_signals(historical_data_cleaned)
-
-# створимо файл для перегляду повного результату
-df_with_signals.to_csv("result.csv", encoding="utf-8")
-
 # тестовий блок, який виконується тільки при прямому запуску цього файлу
 if __name__ == "__main__":
-    # для перевірки виведемо тільки ті дані, коли купуємо або продаємо
-    print("ДНІ З УГОДАМИ")
-    trades = df_with_signals[(df_with_signals['Action'] != 0) & (df_with_signals['Action'].notna())]
-    print(trades[['Close', 'SMA_fast', 'SMA_slow', 'Signal', 'Action']])
+    # проведемо перевірки по різним періодам
+    strategies_to_test = [
+        {"short": 9, "long": 50, "file": "result_9_50.csv"},
+        {"short": 20, "long": 100, "file": "result_20_100.csv"},
+    ]
+    for strat in strategies_to_test:
+        s_period = strat["short"]
+        l_period = strat["long"]
+        file_name = strat["file"]
+
+        # генеруємо сигнали для конкретної пари параметрів
+        df_signals = generate_signals(historical_data_cleaned, short=s_period, long=l_period)
+
+        # зберігаємо результат у CSV
+        df_signals.to_csv(file_name, encoding="utf-8-sig")
+
+        # для перевірки виведемо тільки ті дані, коли купуємо або продаємо
+        print(f"ДНІ З УГОДАМИ ДЛЯ СТРАТЕГІЇ SMA ({s_period} / {l_period})")
+        trades = df_signals[(df_signals['Action'] != 0) & (df_signals['Action'].notna())]
+        print(trades[['Close', 'SMA_fast', 'SMA_slow', 'Signal', 'Action']])
